a504df4b5d5175af3d2af6bf23dd63c6802b973e
Jay Shaffer <jayshaffer@Jays-MacBook-Pro.local>
2018-06-08 16:55:43 -0600

    init

---
+++ react_rails_template.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME=${1:-}
+
+if [ -z "$NAME" ]; then
+  echo "must specify name"
+  exit
+fi
+
+rails new $NAME --webpack=react --database=postgresql -T
+
+cd "./"$NAME
+
+bundle inject byebug "~>10.0.2" --group "development, test"
+bundle inject webmock "~> 3.3.0" --group "test" 
+bundle inject rubocop "~> 0.57.1" --group "development, test" 
+bundle inject bullet "~> 5.7.2" --group "development" 
+bundle inject factory_bot_rails "~> 4.8.2" --group "test" 
+bundle inject rspec-rails "~> 3.7.2" --group "development, test" 
+bundle inject sidekiq "~> 5.0.5"
+
+bundle install
+
+rails generate rspec:install
+
+brew install yarn
+yarn add axios
